d95af834d5de056fe03132e9dce02db6ce46226b
Will Cheng <98398911+weifish0@users.noreply.github.com>
2023-02-03 20:24:00 +0800

    修正錯誤

--- check_in.py
+++ check_in.py
@@ -2,28 +2,27 @@ import genshinstats as gs
 import sys
 
 def get_argv_list(a)-> str:
-    a = a.split()
+    a = a.split("_")
     return a
     
 if __name__ == "__main__":
-    print(sys.argv)
-    # uid_lib = get_argv_list(sys.argv[1])
-    # token_lib = get_argv_list(sys.argv[2])
-    # user_lib = get_argv_list(sys.argv[3])
-    # user_number = len(user_lib)
+    uid_lib = get_argv_list(sys.argv[1])
+    token_lib = get_argv_list(sys.argv[2])
+    user_lib = get_argv_list(sys.argv[3])
+    user_number = len(user_lib)
     
-    # if user_number == 0:
-    #     print("找不到任何使用者")
-    # else:
-    #     for user in range(user_number):
-    #         uid = uid_lib[user]
-    #         token = token_lib[user]
+    if user_number == 0:
+        print("找不到使用者")
+    else:
+        for user in range(user_number):
+            uid = uid_lib[user]
+            token = token_lib[user]
             
-    #         gs.set_cookie(ltuid = uid, ltoken = token)         
-    #         # gs.set_cookie(ltuid=144088752, ltoken="axy7xNLcwx6aFvF0orVhMdBjbk4cCDQK6t0wEDdL")
+            gs.set_cookie(ltuid = uid, ltoken = token)         
+            # gs.set_cookie(ltuid=144088752, ltoken="axy7xNLcwx6aFvF0orVhMdBjbk4cCDQK6t0wEDdL")
 
-    #         reward = gs.claim_daily_reward()
-    #         if reward is not None:
-    #             print(f"{user_lib[user]} 已經自動領取了獎勵 - {reward['cnt']}x {reward['name']}")
-    #         else:
-    #             print(f"{user_lib[user]} 今天已經領取過獎勵了")
+            reward = gs.claim_daily_reward()
+            if reward is not None:
+                print(f"{user_lib[user]} 已經自動領取了獎勵 - {reward['cnt']}x {reward['name']}")
+            else:
+                print(f"{user_lib[user]} 今天已經領取過獎勵了")
